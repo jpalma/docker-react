d75565ea7cf6dffce1fb2b40ab626b9f9a344b64
Justin Palma <jpalma@hey.com>
2021-01-18 15:27:33 -0800

    Changed app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!22aaaabbbaa - justin - dockercompose
+          Hi there! I was changed again.
         </p>
         <a
           className="App-link"
